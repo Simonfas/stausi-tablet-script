68d8906e0a5cb982aea9d2194529486695fb0489
Simonfas <simon.fastrup@gmail.com>
2025-04-08 22:48:27 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 -- Fjern de 2 streger foran '@ox_lib/init.lua' i fxmanifest.lua, hvis du bruger ox_lib --
-Config.oxnotify = true -- Hvis du bruger ox_lib, så sæt denne til true. Ellers lad den være false.
+Config.oxnotify = false -- Hvis du bruger ox_lib, så sæt denne til true. Ellers lad den være false.
 
 Config.politijob = "police" -- Jobnavn
 Config.laegejob = "ambulance" -- Jobnavn
