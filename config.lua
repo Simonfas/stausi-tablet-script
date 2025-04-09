23afcdffa237760dc387096cb1ef5a5498c6b26b
Simonfas <simon.fastrup@gmail.com>
2025-04-09 21:32:50 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,6 +5,16 @@ Config.framework = "VRP" -- Skift mellem "VRP" eller "ESX"
 -- Fjern de 2 streger foran '@ox_lib/init.lua' i fxmanifest.lua, hvis du bruger ox_lib --
 Config.oxnotify = false -- Hvis du bruger ox_lib, så sæt denne til true. Ellers lad den være false.
 
+Config.politiurl = "http://localhost/police/pages/login.php" -- Politilogin URL
+Config.laegeurl = "http://localhost/ems/pages/login.php" -- Lægelogin URL
+
+-- Kun til VRP --
+Config.politimenu = "police" -- Jobnavn
+Config.laegemenu = "ems" -- Jobnavn
+
+Config.politipermission = "police.menu_interaction" -- Jobnavn
+Config.laegepermission = "emergency.menu" -- Jobnavn
+
 -- Kun til ESX --
 Config.politijob = "police" -- Jobnavn
 Config.laegejob = "ambulance" -- Jobnavn
