f096efa772b80686721aa06d833aca7d3e1ad492
Simonfas <simon.fastrup@gmail.com>
2025-04-09 03:55:18 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,7 +5,7 @@ Config.framework = "VRP" -- Skift mellem "VRP" eller "ESX"
 -- Fjern de 2 streger foran '@ox_lib/init.lua' i fxmanifest.lua, hvis du bruger ox_lib --
 Config.oxnotify = false -- Hvis du bruger ox_lib, så sæt denne til true. Ellers lad den være false.
 
-Config.politijob = "Politibetjent" -- Jobnavn
+Config.politijob = "police" -- Jobnavn
 Config.laegejob = "ambulance" -- Jobnavn
 
 Config.politicommand = "tablet" -- Kommando til at åbne tablet for politijobbet
