d7356af305e4e70e33f235496dfb8287381ce66f
Simonfas <simon.fastrup@gmail.com>
2025-04-09 03:53:55 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,11 @@
 Config = {}
+-- Sæt 2 streger foran dependency "vrp" og '@vrp/lib/utils.lua' i fxmanifest.lua, hvis du bruger ESX --
+Config.framework = "VRP" -- Skift mellem "VRP" eller "ESX"
+
 -- Fjern de 2 streger foran '@ox_lib/init.lua' i fxmanifest.lua, hvis du bruger ox_lib --
 Config.oxnotify = false -- Hvis du bruger ox_lib, så sæt denne til true. Ellers lad den være false.
 
-Config.politijob = "police" -- Jobnavn
+Config.politijob = "Politibetjent" -- Jobnavn
 Config.laegejob = "ambulance" -- Jobnavn
 
 Config.politicommand = "tablet" -- Kommando til at åbne tablet for politijobbet
