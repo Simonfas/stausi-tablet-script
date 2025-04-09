42b8ed92fef27200b543f1b090dcf93f3b16d8b7
Simonfas <simon.fastrup@gmail.com>
2025-04-09 03:55:52 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,6 +5,7 @@ Config.framework = "VRP" -- Skift mellem "VRP" eller "ESX"
 -- Fjern de 2 streger foran '@ox_lib/init.lua' i fxmanifest.lua, hvis du bruger ox_lib --
 Config.oxnotify = false -- Hvis du bruger ox_lib, så sæt denne til true. Ellers lad den være false.
 
+-- Kun til ESX --
 Config.politijob = "police" -- Jobnavn
 Config.laegejob = "ambulance" -- Jobnavn
 
