825e16166f71a27ba3d3f3b2677d0ca2a36c773d
Simonfas <simon.fastrup@gmail.com>
2025-04-09 22:55:15 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
--- Sæt 2 streger foran dependency "vrp" og '@vrp/lib/utils.lua' i fxmanifest.lua, hvis du bruger ESX --
-Config.framework = "VRP" -- Skift mellem "VRP" eller "ESX"
+-- Sæt 2 streger foran dependency "vrp" og '@vrp/lib/utils.lua' i fxmanifest.lua, hvis du bruger ESX eller QBCORE --
+Config.framework = "VRP" -- Skift mellem "VRP" eller "ESX" eller "QBCORE"
 
 -- Fjern de 2 streger foran '@ox_lib/init.lua' i fxmanifest.lua, hvis du bruger ox_lib --
 Config.oxnotify = false -- Hvis du bruger ox_lib, så sæt denne til true. Ellers lad den være false.
@@ -15,7 +15,7 @@ Config.laegemenu = "ems" -- Jobnavn
 Config.politipermission = "police.menu_interaction" -- Jobnavn
 Config.laegepermission = "emergency.menu" -- Jobnavn
 
--- Kun til ESX --
+-- Kun til ESX & QBCORE --
 Config.politijob = "police" -- Jobnavn
 Config.laegejob = "ambulance" -- Jobnavn
 
